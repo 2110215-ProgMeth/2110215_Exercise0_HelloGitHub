71393ba12dbf6e08c5fd35ff46728735b8d521ef
asastamp <n_kittiphan@hotmail.com>
2016-08-29 19:11:47 +0700

    Implement greetingMessage() and bubbleSort()

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -13,11 +13,21 @@ public class Hello {
 
 	public static String greetingMessage(String name){
 		// TODO Return string in this format - "Hello, " + given name.
-		return "";
+		return "Hello, "+name;
 	}
 
 	public static int[] bubbleSort(int[] array){
 		// TODO Sort the given array in an ascending order and return the sorted array.
+		int i,j,temp;
+		for (i=0;i<array.length-1;i++){
+			for(j=0;j<array.length-1;j++){
+				if(array[j]>array[j+1]){
+					temp=array[j+1];
+					array[j+1]=array[j];
+					array[j]=temp;
+				}
+			}
+		}
 		return array;
 	}
 	
