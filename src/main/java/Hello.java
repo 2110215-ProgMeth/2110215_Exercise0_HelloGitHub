aa45159f30b326d3246f350d9169bf3cf19e9217
munggumy <iknobkb@gmail.com>
2016-08-30 17:00:58 +0700

    Hello completed
    So easy

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,31 +1,43 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println(Hello.greetingMessage("GitHub"));
-		
-		int[] array = {5,4,3,2,1};
-		
+
+		int[] array = { 5, 4, 3, 2, 1 };
+
 		System.out.println(Hello.arrayToString("Array", array));
 		array = Hello.bubbleSort(array);
 		System.out.println(Hello.arrayToString("Sorted Array", array));
 	}
 
-	public static String greetingMessage(String name){
+	public static String greetingMessage(String name) {
 		// TODO Return string in this format - "Hello, " + given name.
-		return "";
+		return "Hello, " + name;
 	}
 
-	public static int[] bubbleSort(int[] array){
-		// TODO Sort the given array in an ascending order and return the sorted array.
+	public static int[] bubbleSort(int[] array) {
+		// TODO Sort the given array in an ascending order and return the sorted
+		// array.
+		int count = array.length - 1;
+		int token;
+		for(int i = 0; i < array.length - 1; i++){
+			for(int j = 0; j < count; j++){
+				if(array[j] > array[j + 1]){
+					token = array[j];
+					array[j] = array[j+1];
+					array[j+1] = token;
+				}
+			}
+			count--;
+		}
 		return array;
 	}
-	
-	public static String arrayToString(String title,int[] array){
-		
+
+	public static String arrayToString(String title, int[] array) {
+
 		String str = title + " : ";
-		for (int i = 0;i < array.length;i++)
-		{
+		for (int i = 0; i < array.length; i++) {
 			str += array[i] + " ";
 		}
 		return str;
