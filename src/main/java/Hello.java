ee77dc917109b7d9d8a46c54b9263bb954c2fbc1
Wakeme Upwhen <wakemeupwhenyouareready@gmail.com>
2016-09-01 21:32:39 +0700

    Hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -13,11 +13,21 @@ public class Hello {
 
 	public static String greetingMessage(String name){
 		// TODO Return string in this format - "Hello, " + given name.
-		return "";
+		return "Hello, " + name;
 	}
 
 	public static int[] bubbleSort(int[] array){
 		// TODO Sort the given array in an ascending order and return the sorted array.
+		int tmp;
+		for (int i = 0; i < array.length; i++) {
+			for(int j = 0; j < i; j++) {
+				if(array[i] < array[j]) {
+					tmp = array[i];
+					array[i] = array[j];
+					array[j] = tmp;
+				}
+			}
+		}
 		return array;
 	}
 	
